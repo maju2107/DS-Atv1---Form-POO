44e511137650dc85d0963de458a3c63d3cdf4082
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-03-13 17:45:40 -0300

    commit

--- exercicio/Main.java
+++ exercicio/Main.java
@@ -1,5 +1,7 @@
 public class Main{
     public static void main (String[] args){
+//O programa principal contém um menu interativo que oferece as
+// opções de "Cadastrar", "Editar", "Apresentar" ou "Excluir".
         
     }
 }
--- exercicio/Pessoa.java
+++ exercicio/Pessoa.java
@@ -1,3 +1,12 @@
 public class Pessoa(){
+// A classe Pessoa possui atributos como nome, idade e e-mail, além
+// de um construtor para inicializar esses dados e métodos para editar, exibir e
+// excluir as informações.
+    // características da classe
+    String nome;
+    int idade;
+    String email;
+    // funções construtoras
+    // métodos/funções
     
 }
