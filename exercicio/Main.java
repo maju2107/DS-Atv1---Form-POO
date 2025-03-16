73cf449c09a2f27c58cecf0b3cbc9067c3a62bf6
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-03-16 12:28:01 -0300

    add métodos, menu e criei objeto Pessoa

--- exercicio/Main.java
+++ exercicio/Main.java
@@ -1,7 +1,36 @@
+package exercicio;
+import java.util.Scanner;
+
 public class Main{
     public static void main (String[] args){
 //O programa principal contém um menu interativo que oferece as
 // opções de "Cadastrar", "Editar", "Apresentar" ou "Excluir".
+
+    Scanner lerOpcao = new Scanner(System.in);
+    System.out.println("Oie, seja bem vind@ ao sistema de cadastro de Pessoa! Escolha uma operação:");
+    System.out.println("1 - Cadastrar uma nova pessoinha:");
+    System.out.println("2 - Editar os dados de uma pessoinha cadastrada:");
+    System.out.println("3 - Apresentar os dados de uma pessoinha cadastrada:");
+    System.out.println("4 - Excluir os dados de uma pessoinha cadastrada:");
+    System.out.println("5 - Sair do sistema!");
+
+    System.out.println("Digite o número da opção desejada:");
+    int opcao = lerOpcao.nextInt();
+
+    Pessoa pessoa1 = new Pessoa();
+
+    switch(opcao){
+        case 1:
+        break; 
+        case 2:
+        break; 
+        case 3:
+        break; 
+        case 4:
+        break; 
+        case 5:
+        break;  
+    }
         
     }
 }
--- exercicio/Pessoa.java
+++ exercicio/Pessoa.java
@@ -1,3 +1,5 @@
+package exercicio;
+
 public class Pessoa(){
 // A classe Pessoa possui atributos como nome, idade e e-mail, além
 // de um construtor para inicializar esses dados e métodos para editar, exibir e
@@ -7,6 +9,29 @@ public class Pessoa(){
     int idade;
     String email;
     // funções construtoras
+    public Aluno() {}; // construtor padrão
     // métodos/funções
-    
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public cadastrar(){
+
+    }
 }
