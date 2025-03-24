dcdd4db0ccbc840503602101d8537d58bb55f61c
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2025-03-24 15:00:45 -0300

    static

--- exercicio/Pessoa.java
+++ exercicio/Pessoa.java
@@ -37,12 +37,5 @@ public class Pessoa {
         this.email = email;
     }
     
-    public void listaDeOpcoes(){
-    System.out.println("Oie, seja bem vind@ ao sistema de cadastro de Pessoa! Escolha uma operação:");
-    System.out.println("1 - Cadastrar uma nova pessoinha:");
-    System.out.println("2 - Editar os dados de uma pessoinha cadastrada:");
-    System.out.println("3 - Apresentar os dados de uma pessoinha cadastrada:");
-    System.out.println("4 - Excluir os dados de uma pessoinha cadastrada:");
-    System.out.println("5 - Sair do sistema!");
-    }
+   
 }
